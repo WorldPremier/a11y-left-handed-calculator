d7379dd4ff6b7420110c621efc30aef14d4d54e4
Franklyn Reyes <franklynreyes777@yahoo.com>
2022-06-24 01:28:10 -0400

    adding onClick function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./styles.css"
 import { useReducer } from "react"
+import DigitButton from "./DigitButton"
 
-const ACTIONS = {
+ export const ACTIONS = {
   ADD_DIGIT: 'add-digit',
   CHOOSE_OPERATION: 'choose-operation',
   CLEAR: 'clear',
@@ -24,14 +25,15 @@ function reducer(state, {type, payload}){
 function App() {
   //userReducer hook. ;-) 
   const [{currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})// takes empty state
-  dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 }})
+  // dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 }})
   return (
     <div className="calculator-grid">
       <div className="output">
         <div className="previous-operand">{previousOperand}{operation}</div>
         <div className="current-operand">{currentOperand}</div>
       </div>
-      <button>/</button>
+      {/* <DigitButton digit="/" dispatch={dispatch} /> */}
+      <OperationButton digit="/" dispatch={dispatch} />
       <button>DEL</button>
       <button className="span-two">AC</button>
       <button>*</button>
