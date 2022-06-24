e1cea62d542220d0c0fa57dc9cd8f98bb4d85fae
Franklyn Reyes <franklynreyes777@yahoo.com>
2022-06-23 23:33:11 -0400

    Hot fix defect on button number layer to meet left-handed UX

--- src/App.js
+++ src/App.js
@@ -10,18 +10,18 @@ function App() {
       <button>/</button>
       <button>DEL</button>
       <button className="span-two">AC</button>
+      <button>*</button>
       <button>1</button>
       <button>2</button>
       <button>3</button>
-      <button>*</button>
+      <button>+</button>
       <button>4</button>
       <button>5</button>
       <button>6</button>
-      <button>+</button>
+      <button>-</button>
       <button>7</button>
       <button>8</button>
       <button>9</button>
-      <button>-</button>
       <button className="span-two">=</button>
       <button>0</button>
       <button>.</button>
