b00da81759d4fffeb149862e3ecfef5d52340b3b
Franklyn Reyes <franklynreyes777@yahoo.com>
2022-06-24 01:02:53 -0400

    adding operatons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import "./styles.css"
+import { useReducer } from "react"
+
+const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+
+}
+
+function reducer(state, {type, payload}){
+  switch(type) {
+    case ACTIONS.ADD_DIGIT:
+    return {
+      ...state,
+      currentOperand: `${currentOperand || ""}${payload.digit}`
+    }
+  }
+
+}
 
 function App() {
+  //userReducer hook. ;-) 
+  const [{currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})// takes empty state
+  dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 }})
   return (
     <div className="calculator-grid">
-      <div className="output"> a11y 1.0
-        <div className="previous-operand"> Left Handed </div>
-        <div className="current-operand"> Calculator </div>
+      <div className="output">
+        <div className="previous-operand">{previousOperand}{operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button>/</button>
       <button>DEL</button>
