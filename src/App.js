a155d6513484aa07484cb97273756552a6709d93
Franklyn Reyes <franklynreyes777@yahoo.com>
2022-06-30 00:28:23 -0400

    finally fixed import and exports including default yay!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import "./styles.css"
-import { useReducer } from "react"
-import DigitButton from "./DigitButton"
+import { useReducer } from "react";
+import "./styles.css";
+import DigitButton from "./DigitButton";//module has several named ///parameters
+
 
  export const ACTIONS = {
   ADD_DIGIT: 'add-digit',
@@ -11,33 +12,33 @@ import DigitButton from "./DigitButton"
 
 }
 
-function reducer(state, {type, payload}){
+ function reducer(state, {type, payload}){
   switch(type) {
     case ACTIONS.ADD_DIGIT:
     return {
       ...state,
-      currentOperand: `${currentOperand || ""}${payload.digit}`
+      currentOperand: `${state.currentOperand || ""}${payload.digit}`,
     }
   }
 
 }
 
-function App() {
+ function App() {
   //userReducer hook. ;-) 
-  const [{currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})// takes empty state
-  // dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 }})
+  const [{currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
   return (
     <div className="calculator-grid">
       <div className="output">
         <div className="previous-operand">{previousOperand}{operation}</div>
         <div className="current-operand">{currentOperand}</div>
       </div>
-      {/* <DigitButton digit="/" dispatch={dispatch} /> */}
-      <OperationButton digit="/" dispatch={dispatch} />
+      <DigitButton digit="/" dispatch={dispatch} />
+      {/* <OperationButton digit="/" dispatch={dispatch} /> */}
       <button>DEL</button>
       <button className="span-two">AC</button>
+      <operation digit="*" dispatch={dispatch} />      
       <button>*</button>
-      <button>1</button>
+      {/* <DigitButton digit="1" dispatch={dispatch} /> */}
       <button>2</button>
       <button>3</button>
       <button>+</button>
--- src/DigitButton.js
+++ src/DigitButton.js
@@ -1,11 +1,11 @@
 import { ACTIONS } from "./App"
 
 export default function DigitButton({ dispatch, digit }) {
-    return
+    return(
         <button 
-        onClick={() => dispatch({type: ACTIONS.ADD_DIGIT.DIGIT, payload: { digit } })}
+        onClick={() => dispatch({type: ACTIONS.ADD_DIGIT, payload: { digit } })}
         >
             {digit}
         </button>
-    
+    )
 }
