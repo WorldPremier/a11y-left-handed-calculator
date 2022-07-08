33b377c4030567ef98ffc6b0811c95d44dab8814
Franklyn Reyes <franklynreyes777@yahoo.com>
2022-07-07 22:22:29 -0400

    setting functioning operands to evaluate

--- src/App.js
+++ src/App.js
@@ -16,19 +16,85 @@ import OperationButton from "./OperationButton"
  function reducer(state, {type, payload}){
   switch(type) {
     case ACTIONS.ADD_DIGIT:
+      if (payload.digit === "0" && state.currentOperand === "0") {
+        return state
+      }
+      if (payload.digit ==="." && state.currentOperand.includes(".")) {
+        return state
+      }
     return {
       ...state,
       currentOperand: `${state.currentOperand || ""}${payload.digit}`,
     }
-  }
+    case ACTIONS.CHOOSE_OPERATION:
+      //current state set to default by nullifying. 
+      if(state.currentOperand == null && state.previousOperand == null){
+        return state
+      }
+      //current operand is not included as its already slated into current state listed above.
+      //At this point the current state will point to details entered when selecting a number button without
+      //previous state by:
+      //spreading state then setting operations to --> previous state; previous state --> current state
+     
+     
+      // i.e.             ***************CALCULATOR-output-screen*************
+      //                  *                                             56 + *
+      //                  *                                            65945 *
+      //                  ****************************************************
+      // Diagram 1.1
 
+      if (state.previousOperand ==null){
+        return {
+          ...state,
+          operation: payload.operation,
+          previousOperand: state.currentOperand,
+          currentOperand: null,
+        }
+      }
+//setting default actions 
+      return {
+        ...state,
+        previousOperand: evaluate(state),
+        operation: payload.operation,
+        currentOperand: null
+      }
+
+    case ACTIONS.CLEAR:
+      return {}
+
+  }
+}
+
+function evaluate({ currentOperand, previousOperand, operation}){
+  
+  const prev = parseFloat(previousOperand)
+  const current = parseFloat(currentOperand)
+//isNaN = is not a number boolean attr
+  if(isNaN(prev) || isNaN(current) ) return ""
+  let computation = ""
+  switch (operation){
+    case "+":
+      computation = prev + current
+      break
+    case "-":
+      computation = prev - current
+      break
+    case "*":
+      computation = prev * current
+      break
+    case "/":
+      computation = prev / current
+      break
+
+  }
+  return computation.toString()
 }
 
  function App() {
   //userReducer hook. ;-) 
   const [{currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {})
-  //line 30 default way to do it. Removed for enhancement.
-  //dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1} })
+
+    // dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 }})
   return (
     <div className="calculator-grid">
       <div className="output">
